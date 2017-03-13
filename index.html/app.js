7584839a637eae0113d964066b691bbec19bdbf9
Corneliousm808 <corneliousmooring@gmail.com>
2017-03-13 08:45:30 -1000

    not done

---
+++ index.html/app.js
@@ -0,0 +1,8 @@
+console.log("sanitycheck");
+
+//finding html elements
+console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+console.log(document.getElementByTagName("div"))
